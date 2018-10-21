3ad14e5f15b967027446b53c06cd417870ae74a5
Pan Li <incarnation.p.lee@outlook.com>
2018-10-21 17:47:48 +0800

    Add MIT header for each source file.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- dependencies-verify-service/src/main/java/com/microsoft/azure/dependencies/verify/web/VerifyController.java
+++ dependencies-verify-service/src/main/java/com/microsoft/azure/dependencies/verify/web/VerifyController.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
 package com.microsoft.azure.dependencies.verify.web;
 
 import com.microsoft.azure.dependencies.verify.pom.Project;
